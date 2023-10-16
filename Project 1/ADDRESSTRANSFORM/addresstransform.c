3186e43dce480538c8520f88194d1582ed27d1db
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2023-10-16 11:30:38 +0800

    Code Revision & Comment Update

--- Project 1/ADDRESSTRANSFORM/addresstransform.c
+++ Project 1/ADDRESSTRANSFORM/addresstransform.c
@@ -1,3 +1,6 @@
+// objective 1: transform virtual address to physical address
+// objective 2: design a multi-thread program using the new system call to show memory areas shared by threads
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
@@ -29,51 +32,51 @@ unsigned long ADDRESS_TRANSFORM(unsigned long virtual_address) {
     // L1: page global directory
     pgd = pgd_offset(current -> mm, virtual_address);
 
-    printk("pgd_val = 0x%lx, pgd_index = %lu\n", pgd_val(*pgd), pgd_index(virtual_address));
+    printk("pgd_value = 0x%lx, pgd_index = %lu\n", pgd_val(*pgd), pgd_index(virtual_address));
 
     if (pgd_none(*pgd)) {
         printk("map error: page global directory (pgd)\n");
-        return (-1);
+        return 0;
     }
 
     // L2: page p4 directory
     p4d = p4d_offset(pgd, virtual_address);
 
-    printk("p4d_val = 0x%lx, p4d_index = %lu\n", p4d_val(*p4d), p4d_index(virtual_address));
+    printk("p4d_value = 0x%lx, p4d_index = %lu\n", p4d_val(*p4d), p4d_index(virtual_address));
 
     if (p4d_none(*p4d)) {
         printk("map error: page p4 directory (p4d)\n");
-        return (-1);
+        return 0;
     }
 
     // L3: page upper directory
     pud = pud_offset(p4d, virtual_address);
 
-    printk("pud_val = 0x%lx, pud_index = %lu\n", pud_val(*pud), pud_index(virtual_address));
+    printk("pud_value = 0x%lx, pud_index = %lu\n", pud_val(*pud), pud_index(virtual_address));
 
     if (pud_none(*pud)) {
         printk("map error: page upper directory (pud)\n");
-        return (-1);
+        return 0;
     }
 
     // L4: page middle directory
     pmd = pmd_offset(pud, virtual_address);
 
-    printk("pmd_val = 0x%lx, pmd_index = %lu\n", pmd_val(*pmd), pmd_index(virtual_address));
+    printk("pmd_value = 0x%lx, pmd_index = %lu\n", pmd_val(*pmd), pmd_index(virtual_address));
 
     if (pmd_none(*pmd)) {
         printk("map error: page middle directory (pmd)\n");
-        return (-1);
+        return 0;
     }
 
     // L5: page table entry
     pte = pte_offset_map(pmd, virtual_address);
 
-    printk("pte_val = 0x%lx, ptd_index = %lu\n", pte_val(*pte), pte_index(virtual_address));
+    printk("pte_value = 0x%lx, ptd_index = %lu\n", pte_val(*pte), pte_index(virtual_address));
 
     if (pte_none(*pte)) {
         printk("map error: page table entry (pte)\n");
-        return (-1);
+        return 0;
     }
 
     struct page *result_page = pte_page(*pte);
@@ -88,21 +91,29 @@ unsigned long ADDRESS_TRANSFORM(unsigned long virtual_address) {
     return physical_address;
 }
 
-SYSCALL_DEFINE4(my_get_physical_addresses, unsigned long*, initial, int, len_vir, unsigned long*, result, int, len_phy) {
-    unsigned long virtual_address[len_vir];
-    unsigned long physical_address[len_vir];
+// syscall definition using self-written function
+SYSCALL_DEFINE4(my_get_physical_addresses,
+                unsigned long*, initial,
+                int, virtual_address_length,
+                unsigned long*, result,
+                int, physical_address_length) {
+    unsigned long virtual_address[virtual_address_length];
+    unsigned long physical_address[virtual_address_length];
 
-    long vir_copy = copy_from_user(virtual_address, initial, sizeof(unsigned long)*len_vir);
+    // original: unsigned long copy_from_user(void * to, const void __user * from, unsigned long n);
+    //
+    long copy_virtual_address = copy_from_user(virtual_address, initial, sizeof(unsigned long)*virtual_address_length);
 
-    printk("len_vir = %d", len_vir);
+    printk("virtual_address_length = %d", virtual_address_length);
 
-    for (i = 0; i < len_vir; i++) {
+    for (i = 0; i < virtual_address_length; i++) {
         printk("i = %d", i);
         physical_address[i] = ADDRESS_TRANSFORM(virtual_address[i]);
     }
 
-    long phy_copy = copy_to_user(result, physical_address, sizeof(unsigned long)*len_vir);
-    long phy_len_copy = copy_to_user(&len_phy, &len_vir, sizeof(int));    
+    // original: unsigned long copy_to_user(void __user *to, const void *from, unsigned long n);
+    long copy_physical_address = copy_to_user(result, physical_address, sizeof(unsigned long)*virtual_address_length);
+    long copy_physical_length = copy_to_user(&physical_address_length, &virtual_address_length, sizeof(int));
 
     return 0;
 }
