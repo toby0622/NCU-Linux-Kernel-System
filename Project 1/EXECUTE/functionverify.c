c45559004d16e9c13fe46a54c976d98c2a3ee55f
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2023-10-17 17:39:48 +0800

    Code Revision

--- Project 1/EXECUTE/functionverify.c
+++ Project 1/EXECUTE/functionverify.c
@@ -1,3 +1,5 @@
+// address result checker: collaborate with "devmem" function
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -5,16 +7,22 @@
 #include <sys/types.h>
 #include <syscall.h>
 
+#define TRUE 1
+#define FALSE 0
+
 int bss_value;
-int data_value = 123;
+int data_value = 123456789;
+
 int code_function() {
     return 0;
 }
 
 int main() {
     sleep(2);
+
     int block_input;
     int stack_value = 10;
+
     unsigned long stack = (unsigned long)&stack_value;
     unsigned long lib = (unsigned long)getpid;
     unsigned long heap = (unsigned long)malloc(sizeof(int));
@@ -22,30 +30,29 @@ int main() {
     unsigned long data = (unsigned long)&data_value;
     unsigned long code = (unsigned long)code_function;
 
-    int len = 6;
-    unsigned long vir_addrs[6] = {stack, lib, heap, bss, data, code};
-    unsigned long phy_addrs[6];
-    long copy = syscall(548, vir_addrs, len, phy_addrs, len);
+    int length = 6;
+
+    unsigned long virtual_address[6] = {stack, lib, heap, bss, data, code};
+    unsigned long physical_address[6];
+
+    long copy = syscall(548, virtual_address, length, physical_address, length);
     
     if (copy < 0) {
-        printf("address transfer failed!!");
+        printf("transfer error: address invalid");
         exit(1);
     }
 
-    while(1) {
-        printf("block...");
-        
+    while(TRUE) {
         scanf("%d", &block_input);
 
-        printf("============= main =============\n");
         printf("pid = %d\n", (int)getpid());
         printf("segment\tvalue\tvir_addr\tphy_addr\n");
-        printf("stack\t%d\t%lx\t%lx\n", stack_value, vir_addrs[0], phy_addrs[0]);
-        printf("getpid\t%d\t%lx\t%lx\n", (int)getpid(), vir_addrs[1], phy_addrs[1]);
-        printf("heap\t%d\t%lx\t%lx\n", *(int*)heap, vir_addrs[2], phy_addrs[2]);
-        printf("bss\t%d\t%lx\t%lx\n", bss_value, vir_addrs[3], phy_addrs[3]);
-        printf("data\t%d\t%lx\t%lx\n", data_value, vir_addrs[4], phy_addrs[4]);
-        printf("code\t%d\t%lx\t%lx\n", code_function(), vir_addrs[5], phy_addrs[5]);
+        printf("stack\t%d\t%lx\t%lx\n", stack_value, virtual_address[0], physical_address[0]);
+        printf("getpid\t%d\t%lx\t%lx\n", (int)getpid(), virtual_address[1], physical_address[1]);
+        printf("heap\t%d\t%lx\t%lx\n", *(int*)heap, virtual_address[2], physical_address[2]);
+        printf("bss\t%d\t%lx\t%lx\n", bss_value, virtual_address[3], physical_address[3]);
+        printf("data\t%d\t%lx\t%lx\n", data_value, virtual_address[4], physical_address[4]);
+        printf("code\t%d\t%lx\t%lx\n", code_function(), virtual_address[5], physical_address[5]);
     }
 
     return 0;
